dc5e6c51e022b30114faa3ec6865b88cfe80aa0d
gounane <gounane.abderrahim@gmail.com>
2014-03-06 18:04:44 +0000

    merge forms and tables

---
+++ js/tables.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+	$(".widget-collapse").click(function(){
+		icone = $(this).children('i');
+		content = $(this).parents('.widget-box').find('.widget-content');
+		if(icone.hasClass('fa-minus')){
+			icone.removeClass('fa-minus');
+			icone.addClass('fa-plus');
+			content.slideUp('slow');
+		}else{
+			icone.removeClass('fa-plus');
+			icone.addClass('fa-minus');
+			content.slideDown('slow');
+		}
+		return false;
+	});
+
+	$("#example").dataTable({
+	  "aaData":[
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	    ["Flippa","http://flippa.com","Marketplace","null"],
+	    ["99designs","http://99designs.com","Marketplace","null"],
+	    ["Learnable","http://learnable.com","Online courses","null"],
+	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"],
+
+
+	  ],
+	  "aoColumnDefs":[{
+	        "sTitle":"Site name"
+	      , "aTargets": [ "site_name" ]
+	  },{
+	        "aTargets": [ 1 ]
+	      , "bSortable": false
+	      , "mRender": function ( url, type, full )  {
+	          return  '<a href="'+url+'">' + url + '</a>';
+	      }
+	  },{
+	        "aTargets":[ 3 ]
+	      , "sType": "date"
+	      , "mRender": function(date, type, full) {
+	          return (full[2] == "Blog")
+	                    ? new Date(date).toDateString()
+	                    : "N/A" ;
+	      } 
+	  }],
+	  "sPaginationType": "full_numbers",
+	  "bJQueryUI": true,
+	  "bStateSave": true
+	});
+});
