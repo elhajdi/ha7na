07714f2b239f6316c841d1ecbb9bfa7fbb51dcd3
elhajdi <elhajdi@neptis.info>
2014-03-06 18:07:38 +0000

    simplest template forms

---
+++ js/simplest-forms.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+/*	
+	$('input[type=checkbox],input[type=radio]').iCheck({
+    	checkboxClass: 'icheckbox_flat-blue',
+    	radioClass: 'iradio_flat-blue'
+	});
+*/
+	$('select.bs-select2').select2();	
+	
+    $('.colorpicker').colorpicker();
+    $('.datepicker').datepicker();
+});
